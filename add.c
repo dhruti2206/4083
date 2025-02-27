4562bbfd3609b748994ed9a867b67e6b9b36cd6b
Meenaamruthachilla <chilla.meenaamrutha@s.amity.edu>
2025-02-27 11:30:07 +0530

    added division of numbers

--- add.c
+++ add.c
@@ -1,12 +1,13 @@
 #include<stdio.h>
 void main()
 {
-	int a=5,b=2,c,d,e;
+	int a=5,b=2,c,d,e,f;
 	c=a+b;
 	printf("the sum of the numbers is:%d",c);
 	d=a-b;
 	printf("the difference of numbers is:%d",d);
         e=a*b;
 	printf("the product of the numbers is:%d",e);
-
+        f=a/b;
+	printf("the division of the numbers is:%d",f);
 }
