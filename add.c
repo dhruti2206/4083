ab0e76cce0983a0815dc01b54522198ae71514cf
Meenaamruthachilla <chilla.meenaamrutha@s.amity.edu>
2025-02-27 11:27:02 +0530

    added product of the numbers

--- add.c
+++ add.c
@@ -1,10 +1,12 @@
 #include<stdio.h>
 void main()
 {
-	int a=5,b=2,c,d;
+	int a=5,b=2,c,d,e;
 	c=a+b;
 	printf("the sum of the numbers is:%d",c);
 	d=a-b;
 	printf("the difference of numbers is:%d",d);
+        e=a*b;
+	printf("the product of the numbers is:%d",e);
 
 }
