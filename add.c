893c70d5d07a346f886c70107329e7c16252e642
Meenaamruthachilla <chilla.meenaamrutha@s.amity.edu>
2025-02-27 11:22:56 +0530

    added difference of numbers

--- add.c
+++ add.c
@@ -1,8 +1,10 @@
 #include<stdio.h>
 void main()
 {
-	int a=5,b=2,c;
+	int a=5,b=2,c,d;
 	c=a+b;
 	printf("the sum of the numbers is:%d",c);
-	return 0;
+	d=a-b;
+	printf("the difference of numbers is:%d",d);
+
 }
